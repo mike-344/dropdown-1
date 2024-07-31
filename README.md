b610bd6616f2fdd48f18e967beff6de405cf2cb5
Michael Sullivan <sullivanmichael344@gmail.com>
2024-07-30 23:40:01 -0400

    Setup dropdown function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-template
+# dropdown-menu
+
+A simple dropdown menu package in vanilla HTML, CSS, and JS. Takes a button and a menu and toggles the menu content on button click. Initially sets content to "hidden"
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-import '../css/style.css'
+import "../css/style.css";
+
+const toggle = document.querySelector(".dropdown-button");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+
+
+
+function toggleDropdown(toggleButton, menu){
+    menu.style.visibility = "hidden"
+    toggleButton.addEventListener("click", () => {
+        if (menu.style.visibility === "hidden"){
+            menu.style.visibility = "visible"
+        } else {
+            menu.style.visibility = "hidden";
+        }
+    })
+}
+
+toggleDropdown(toggle, dropdownContent);
+
+export {toggleDropdown}
